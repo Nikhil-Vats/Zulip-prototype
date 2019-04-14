94807f60c9e7fac829e7bddf38b09fdc8b8ad5fd
Nikhil-Vats <f20170473@pilani.bits-pilani.ac.in>
2019-04-14 17:27:09 +0530

    added js for sidenav

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function showSidenav() {
+    document.getElementById('sidenav').classList.add('show');
+}
+
+function closeSidenav() {
+    document.getElementById('sidenav').classList.remove('show');
+}
