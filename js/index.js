cc9cc78dd6c00b94f5a8946a4de5224e01ee9b37
Nikhil-Vats <f20170473@pilani.bits-pilani.ac.in>
2019-04-23 09:57:35 +0530

    Updated sidenav css

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
+document.getElementsByClassName('sidenav-logo')[0].style.display = 'none';
+window.onresize = resize;
+
+function resize()
+{
+    var w = window, d = document, e = d.documentElement,
+    g = d.getElementsByTagName('body')[0];
+ console.log(w.innerWidth);   
+ if(w.innerWidth > 500 || e.clientWidth > 500 || g.clientWidth > 500)
+ document.getElementsByClassName('sidenav-logo')[0].style.display = 'none';
+ else 
+ document.getElementsByClassName('sidenav-logo')[0].style.display = 'flex';
+}
 function showSidenav() {
+    console.log('clicked');
     document.getElementById('sidenav').classList.add('show');
+    document.getElementsByClassName('sidenav-logo')[0].style.display = 'flex';
 }
 
 function closeSidenav() {
+    document.getElementsByClassName('sidenav-logo')[0].style.display = 'none';
     document.getElementById('sidenav').classList.remove('show');
 }
